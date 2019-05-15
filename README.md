63003441646531ac3bb7a226038df1a7150dd411
azukisiromochi <azukisiromochi@gmail.com>
2019-05-15 15:21:07 +0900

    とりあえず動くところまで

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
-# [WIP] p15n
-p15nとはPrefectureization（都道府県化）のことで、簡単に『方言』が使えるライブラリです。
+# p15n.js
+p15n.jsとはPrefectureization（都道府県化）のことで、簡単に『方言』が使えるライブラリです。
+
+## Installation
+
+ブラウザから:
+
+```html
+<script src="p15n.js"></script>
+```
+
+[WIP] npmを使う:
+
+```shell
+$ npm install p15n.js
+```
+
+## Usage
+
+コールバック関数を渡す方法:
+
+```javascript
+import p15n from 'p15n';
+
+p15n.init({
+  pref: '鳥取県',
+  resources: {
+    '鳥取県': {
+      'title': 'p15nライブラリを作ってみたにぃ！　すごいだらぁ！？',
+      'description': 'p15n.jsは簡単に『方言』が使えるライブラリだにぃ。'
+    },
+  }
+}, function(t) {
+  // initialized and ready to go!
+  document.getElementById('title').textContent = p15n.t('title');
+  document.getElementById('description').textContent = p15n.t('description');
+});
+```
+
+Promiseを使う方法:
+
+```javascript
+import p15n from 'p15n';
+
+p15n.init({
+  pref: '鳥取県',
+  resources: {
+    '鳥取県': {
+      'title': 'p15nライブラリを作ってみたにぃ！　すごいだらぁ！？',
+      'description': 'p15n.jsは簡単に『方言』が使えるライブラリだにぃ。'
+    },
+  }
+}).then(() => {
+  // initialized and ready to go!
+  document.getElementById('title').textContent = p15n.t('title');
+  document.getElementById('description').textContent = p15n.t('description');
+});
+```
+
+## License
+
+p15n.js is created under the [MIT](https://opensource.org/licenses/MIT) license.
---
+++ src/p15n.js
@@ -0,0 +1,69 @@
+
+class P15n {
+  constructor(options = {}, callback) {
+
+    this.init(options, callback);
+  }
+
+  init(options = {}, callback) {
+
+    if (typeof options === 'function') {
+      callback = options;
+      options = {};
+    }
+
+    this.pref = options.pref || '東京都';
+    this.fallbackPref = options.fallbackPref || '東京都';
+    this.defaultPref = '東京都';
+    this.resources = options.resources || {}; //TODO: JSONファイルを参照するように変更
+
+    const deferred = this._defer();
+
+    wait(0).then(() => {
+      deferred.resolve(this.t);
+      if (typeof callback === 'function') callback(this.t);
+    });
+
+    return deferred;
+  }
+
+  t(key) {
+
+    if (!this.exists(key)) return '';
+
+    const resource = this.resources[this.pref] || this.resources[this.fallbackPref] || this.resources[this.defaultPref];
+
+    return resource[key];
+  }
+
+  exists(key) {
+
+    if (!this.resources) return false;
+
+    const resource = this.resources[this.pref] || this.resources[this.fallbackPref] || this.resources[this.defaultPref];
+
+    if (!resource) return false;
+
+    return resource[key] && resource[key] !== undefined;
+  }
+
+  _defer() {
+    let res;
+    let rej;
+
+    const promise = new Promise((resolve, reject) => {
+      res = resolve;
+      rej = reject;
+    });
+
+    promise.resolve = res;
+    promise.reject = rej;
+
+    return promise;
+  }
+
+};
+
+const wait = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+export default new P15n();
